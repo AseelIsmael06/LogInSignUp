08fcefdfd3adf8c2cc94dd45bfc5e7f9916eb1be
aseel_vobqnm4 <Aseelism06@gnail.com>
2022-11-04 09:49:54 +0200

    gfg

---
+++ app/src/main/java/com/example/signupsignin/LogInFragment.java
@@ -0,0 +1,122 @@
+package com.example.signupsignin;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LogInFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LogInFragment extends Fragment {
+    private View objectSignInFragment;
+    private Button logInBtn;
+    private EditText mailEt,passEt;
+    private FirebaseAuth mAuth;
+    private TextView logInToSignUpTxt;
+
+    private void attachComponents(){
+        logInBtn=objectSignInFragment.findViewById(R.id.btnLogIn);
+        mailEt=objectSignInFragment.findViewById(R.id.etEmail);
+        passEt=objectSignInFragment.findViewById(R.id.etPassword);
+        mAuth=FirebaseAuth.getInstance();
+        logInToSignUpTxt=objectSignInFragment.findViewById(R.id.etText3);
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logInUser();
+                ForgotPasswordFragment forgotPasswordFragment=new ForgotPasswordFragment();
+                FragmentManager manager=getFragmentManager();
+                manager.beginTransaction()
+                        .replace(R.id.frameLayoutMain,forgotPasswordFragment,forgotPasswordFragment.getTag())
+                        .commit();
+            }
+        });
+    }
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+
+    public LogInFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public LogInFragment newInstance(String param1, String param2) {
+        LogInFragment fragment = new LogInFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    private void logInUser(){
+        try{
+            if(!mailEt.getText().toString().isEmpty()&&!passEt.getText().toString().isEmpty()){
+                if(mAuth!=null){
+                    mAuth.signInWithEmailAndPassword(mailEt.getText().toString(),passEt.getText().toString())
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    Toast.makeText(getContext(), "User signed in successfully.", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
+            else{
+                Toast.makeText(getContext(), "Missing fields identified.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e){
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        objectSignInFragment=inflater.inflate(R.layout.fragment_log_in2,container,false);
+        attachComponents();
+
+        return objectSignInFragment;
+    }
+
+}
---
+++ app/src/main/java/com/example/signupsignin/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.signupsignin;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseServices fs=new FirebaseServices();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayoutMain, new LogInFragment());
+        ft.commit();
+    }
+
+    public void goToSignUp(View view){
+        FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayoutMain,new SignUpFragment());
+        ft.commit();
+    }
+}
